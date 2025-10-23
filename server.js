99d82965e801a00099d90d4f996d75952b27060e
Wallace Venancio <wallacefvenancio@gmail.com>
2025-10-23 16:18:15 -0300

    Adição do framework express na pasta src

--- server.js
+++ server.js
@@ -1,19 +1,9 @@
 //http biblioteca nativa do node
-import http from "http";
+//import http from "http";
 
+import app from "./src/app.js";
 const PORT = 3000;
 
-const rotas ={
-    "/": "Curso de Node.js",
-    "/livros": "Entrei na rota livros",
-    "/autores": "Entrei na rota autores",
-}; 
-
-const server = http.createServer((req, res) => {
-    res.writeHead(200, {"Content-Type": "text/plan"});
-    res.end(rotas[req.url]);
-});
-
-server.listen(PORT, () => {
+app.listen(PORT, () => {
     console.log("Servidor escutando!")
 });
