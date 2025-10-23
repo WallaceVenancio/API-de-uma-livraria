f6112b104253705f0190231b0c0bcc3b9c3ef1d7
Wallace Venancio <wallacefvenancio@gmail.com>
2025-10-23 16:28:18 -0300

    Criação da pagina /livros

--- src/app.js
+++ src/app.js
@@ -1,8 +1,23 @@
 import express from "express";
+
 const app = express();
+
+const livros = [{
+    id: 1,
+    titulo: "Harry Potter"
+},
+{
+    id: 2,
+    titulo: "Dom Casmurro"
+}
+]
 
 app.get("/", (req, res)=> {
     res.status(200).send("Curso de Node.js");
+});
+
+app.get("/livros", (req, res) => {
+    res.status(200).json(livros);
 });
 
 export default app;
