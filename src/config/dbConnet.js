6895757f35c36281b3a48aaea4bbfde63848f1fe
Wallace Venancio <wallacefvenancio@gmail.com>
2025-11-04 16:55:27 -0300

    Correção do bug do servidor não conectar

--- src/config/dbConnet.js
+++ src/config/dbConnet.js
@@ -1,7 +1,7 @@
 import mongoose, { mongo } from "mongoose";
 
 async function conectaNaDataBase() {
-    mongoose.connect("mongodb+srv://admin:<admin123>@cluster0.mongodb.net/Livraria?retryWrites=true&w=majority");
+    mongoose.connect("mongodb+srv://admin:admin123@cluster0.h0jkz5i.mongodb.net/livraria=Cluster0");
 
     return mongoose.connection
     
