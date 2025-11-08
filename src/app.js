665cf1eb9d057941512196e6d0e4db2c91db8fc9
Wallace Venancio <wallacefvenancio@gmail.com>
2025-11-08 18:36:43 -0300

    Criação da pasta routes e do controller

--- src/app.js
+++ src/app.js
@@ -17,10 +17,6 @@ const app = express();
 //middleware
 app.use(express.json());
 
-app.get("/livros", async (req, res)=> {
-    const listaLivros = await livro.find({})
-    res.status(200).send(listaLivros);
-});
 
 app.get("/livros", (req, res) => {
     res.status(200).json(livros);
