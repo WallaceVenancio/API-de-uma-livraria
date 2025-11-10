e107ee104c6ff7f9ea9819b2626f0ce6b203090b
Wallace Venancio <wallacefvenancio@gmail.com>
2025-11-10 16:03:46 -0300

    Alteração do CRUD

--- src/routes/livrosRoutes.js
+++ src/routes/livrosRoutes.js
@@ -4,5 +4,9 @@ import LivroController from "../controllers/livroController.js";
 const routes = express.Router()
 
 routes.get("/livros", LivroController.listarLivros)
+routes.get("/livros/:id", LivroController.listarLivroPorId)
+routes.get("/livros", LivroController.cadastraLivro)
+routes.get("/livros/:id", LivroController.atualizarLivro)
+
 
 export default routes;
